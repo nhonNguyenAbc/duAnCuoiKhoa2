bd4c9d4ac73e2a2da47649678fd4f32996af3149
nhonNguyenAbc <nguyenthanhnhonabc@gmail.com>
2024-01-14 15:35:35 +0700

    update3

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,31 +1,31 @@
 import React, { useState } from "react";
 import Rating from "./Sidebar/rating";
-
+import ReactStars from "react-rating-stars-component";
 const reviwtitle = "Add a Review";
 let ReviewList = [
   {
-    imgUrl: "/src/assets/images/instructor/01.jpg",
+    imgUrl: "/images/instructor/01.jpg",
     imgAlt: "Client thumb",
     name: "Ganelon Boileau",
     date: "Posted on Jun 10, 2022 at 6:57 am",
     desc: "Enthusiast build innovativ initiatives before lonterm high-impact awesome theme seo psd porta monetize covalent leadership after without resource.",
   },
   {
-    imgUrl: "/src/assets/images/instructor/02.jpg",
+    imgUrl: "/images/instructor/02.jpg",
     imgAlt: "Client thumb",
     name: "Morgana Cailot",
     date: "Posted on Jun 10, 2022 at 6:57 am",
     desc: "Enthusiast build innovativ initiatives before lonterm high-impact awesome theme seo psd porta monetize covalent leadership after without resource.",
   },
   {
-    imgUrl: "/src/assets/images/instructor/03.jpg",
+    imgUrl: "/images/instructor/03.jpg",
     imgAlt: "Client thumb",
     name: "Telford Bois",
     date: "Posted on Jun 10, 2022 at 6:57 am",
     desc: "Enthusiast build innovativ initiatives before lonterm high-impact awesome theme seo psd porta monetize covalent leadership after without resource.",
   },
   {
-    imgUrl: "/src/assets/images/instructor/04.jpg",
+    imgUrl: "/images/instructor/04.jpg",
     imgAlt: "Client thumb",
     name: "Cher Daviau",
     date: "Posted on Jun 10, 2022 at 6:57 am",
@@ -50,7 +50,6 @@ const Review = () => {
           Reviews 4
         </li>
       </ul>
-      
       <div
         className={`review-content ${
           reviewShow ? "review-content-show" : "description-show"
@@ -93,7 +92,13 @@ const Review = () => {
                 <div className="col-md-4 col-12">
                   <div className="rating">
                     <span className="rating-title">Your Rating : </span>
-                    <Rating />
+                    <ReactStars
+                      count={5}
+                      size={24}
+                      value={0}
+                      edit={true}
+                      activeColor="#ffd700"
+                    />
                   </div>
                 </div>
                 <div className="col-md-12 col-12">
@@ -125,7 +130,7 @@ const Review = () => {
           </p>
           <div className="post-item">
             <div className="post-thumb">
-              <img src="/src/assets/images/shop/01.jpg" alt="shop" />
+              <img src="/images/shop/01.jpg" alt="shop" />
             </div>
             <div className="post-content">
               <ul className="lab-ul">
--- src/components/Sidebar/Rating.jsx
+++ src/components/Sidebar/Rating.jsx
@@ -3,11 +3,26 @@ import React from "react";
 const Rating = () => {
   return (
     <span className="ratting">
-      <i className="icofont-ui-rating"></i>
-      <i className="icofont-ui-rating"></i>
-      <i className="icofont-ui-rating"></i>
-      <i className="icofont-ui-rating"></i>
-      <i className="icofont-ui-rating"></i>
+      <i
+        className="icofont-ui-rating"
+        style={{ color: "rgb(255, 215, 0)" }}
+      ></i>
+      <i
+        className="icofont-ui-rating"
+        style={{ color: "rgb(255, 215, 0)" }}
+      ></i>
+      <i
+        className="icofont-ui-rating"
+        style={{ color: "rgb(255, 215, 0)" }}
+      ></i>
+      <i
+        className="icofont-ui-rating"
+        style={{ color: "rgb(255, 215, 0)" }}
+      ></i>
+      <i
+        className="icofont-ui-rating"
+        style={{ color: "rgb(255, 215, 0)" }}
+      ></i>
     </span>
   );
 };
